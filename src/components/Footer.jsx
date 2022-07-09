f8a93e803b1d5423640f6356240c82211451bd2d
Edwin Roosevelt <bedwinroosevelt@gmail.com>
2022-07-10 01:01:39 +0530

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,18 +2,19 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-      <div className="container">
-        <div className="col-md-4 d-flex align-items-center">
+    <footer className="py-3 border-top">
+        <div className="d-flex justify-content-center align-items-center">
           <a
             href="https://www.edwinroosevelt.com"
             target="_blank"
-            className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
+            className="text-muted text-decoration-none"
           >
-            <span className="text-muted">&nbsp; &copy; 2021 Edwin Roosevelt</span>
+            <span className="text-muted">
+              &nbsp; &copy; 2021 Edwin Roosevelt
+            </span>
           </a>
         </div>
-      </div>
+
     </footer>
   );
 }
